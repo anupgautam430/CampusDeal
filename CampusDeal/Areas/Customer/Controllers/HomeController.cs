2f8b0ae78b9ce92ea88f5cd543c601afa15e04c7
Anup Gautam <anupgautam430@gmail.com>
2023-10-12 20:42:41 +0545

    bot

--- CampusDeal/Areas/Customer/Controllers/HomeController.cs
+++ CampusDeal/Areas/Customer/Controllers/HomeController.cs
@@ -76,6 +76,12 @@ namespace CampusDeal.Areas.Customer.Controllers
             return View();
         }
 
+        public IActionResult Chatbot()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
